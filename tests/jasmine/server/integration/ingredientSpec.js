952c4898ad46eb150fc9258a84f91cef968dad46
unknown <patern.rival@gmail.com>
2015-10-05 07:52:15 +0200

    tests ingredients

--- tests/jasmine/server/integration/ingredientSpec.js
+++ tests/jasmine/server/integration/ingredientSpec.js
@@ -44,50 +44,50 @@ describe('Ingredients', function() {
     /**
     * Teste findOrSave dans le cas find.
     */
-    // it('findOrSave cas find', function() {
-    //   spyOn(Ingredients, 'insert').and.callThrough();
-    //   Meteor.call('ingredient/findOrSave', 'pomme', function(error, result) {
-    //     expect(error).toBe(undefined);
-    //     expect(result).not.toBe(undefined);
-    //     expect(result).not.toBe(null);
-    //     expect(Ingredients.insert).not.toHaveBeenCalled();
-    //   });
-    // });
+    it('findOrSave cas find', function() {
+      spyOn(Ingredients, 'insert');
+      Meteor.call('ingredient/findOrSave', 'pomme', function(error, result) {
+        expect(error).toBe(undefined);
+        expect(result).not.toBe(undefined);
+        expect(result).not.toBe(null);
+        expect(Ingredients.insert).not.toHaveBeenCalled();
+      });
+    });
 
     /**
     * Teste findOrSave dans le cas save.
     */
-    // it('findOrSave cas save', function() {
-    //   spyOn(Ingredients, 'insert').and.callThrough();
-    //   Meteor.call('ingredient/findOrSave', 'poire', function(error, result) {
-    //     expect(error).toBe(undefined);
-    //     expect(result).not.toBe(undefined);
-    //     expect(result).not.toBe(null);
-    //     expect(Ingredients.insert).toHaveBeenCalled();
-    //   });
-    // });
+    it('findOrSave cas save', function() {
+      spyOn(Ingredients, 'insert');
+      Meteor.call('ingredient/findOrSave', 'poire', function(error, result) {
+        expect(error).toBe(undefined);
+        expect(result).not.toBe(undefined);
+        expect(result).not.toBe(null);
+        expect(Ingredients.insert).toHaveBeenCalled();
+      });
+    });
 
     /**
     * Teste findOrSave dans le cas save empty.
     */
-    // it('findOrSave cas save', function() {
-    //   spyOn(Ingredients, 'insert').and.callThrough();
-    //   Meteor.call('ingredient/findOrSave', '', function(error, result) {
-    //     expect(error).not.toBe(undefined);
-    //     expect(result).toBe(undefined);
-    //     expect(Ingredients.insert).not.toHaveBeenCalled();
-    //   });
-    // });
+    it('findOrSave cas save', function() {
+      spyOn(Ingredients, 'insert');
+      Meteor.call('ingredient/findOrSave', '', function(error, result) {
+        expect(error).not.toBe(undefined);
+        expect(result).toBe(undefined);
+        expect(Ingredients.insert).not.toHaveBeenCalled();
+      });
+    });
 
     /**
     * Teste la récupération d'un ingrédient non existant.
     */
-    // it('findByLibelle non exists', function() {
-    //   Meteor.call('ingredient/findByLibelle', 'fraise', function(error, result) {
-    //     expect(error).toBe(undefined);
-    //     expect(result).toBe(undefined);
-    //   });
-    // });
+    it('findByLibelle non exists', function() {
+      Meteor.call('ingredient/findByLibelle', 'fraise', function(error, result) {
+        expect(error).toBe(undefined);
+        expect(result).toBe(undefined);
+      });
+    });
 
     /**
     * Teste la récupération d'un ingrédient existant.
